eaf088be390750cd1f711b811b2ec0342361cfa8
veypi <i@veypi.com>
2022-06-05 16:05:09 +0800

    update

--- proc-macro-workshop/builder/src/lib.rs
+++ proc-macro-workshop/builder/src/lib.rs
@@ -1,10 +1,10 @@
 use proc_macro::TokenStream;
 use syn;
 
-#[proc_macro_derive(Builder)] // 注意，这里和第一篇文章里的 #[proc_macro_attribute]不同
+#[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     eprintln!("{:#?}", input);
-    // let st = syn::parse_macro_input!(input as syn::DeriveInput);
-    // eprintln!("{:#?}", st);
+    let st = syn::parse_macro_input!(input as syn::DeriveInput);
+    eprintln!("{:#?}", st);
     TokenStream::new()
 }
