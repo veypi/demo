4bec2e4ad86a778c19d33dc607afa172a36475c7
veypi <i@veypi.com>
2022-06-06 00:38:30 +0800

    builder 04

--- proc-macro-workshop/builder/src/lib.rs
+++ proc-macro-workshop/builder/src/lib.rs
@@ -3,11 +3,8 @@
 // Copyright (C) 2022 veypi <veypi@qq.com>
 // 2022-06-05 17:33
 // Distributed under terms of the MIT license.
-// 02 中使用字符串追加的方法比较不方便， 所以本节之后采用syn和quote 库
-// 再次感谢大佬 dtolnay
-// [syn](https://github.com/dtolnay/syn) 是一个将 ident数组 (TokenStream.标记流) 转换成ast 语法树的解析库
-// [quote](https://github.com/dtolnay/quote) 可以用来辅助将rust 语法树 转换成标记流(TokenStream).
-// 插值语法 #var     变量 var 要实现特征 quote::ToTokens
+// 03 主要是为 CommandBuilder 实现build方法
+// 涉及到特征对象 Box<dyn Error>> 中 Error 特征未导入
 
 use proc_macro::TokenStream;
 use quote::quote;
@@ -21,13 +18,29 @@ pub fn derive(input: TokenStream) -> TokenStream {
     //
     // 编写 CommandBuilder 结构体 及其方法
     let command_builder = quote! {
-    pub struct CommandBuilder {
+    use std::error::Error;
+    struct CommandBuilder {
         executable: Option<String>,
         args: Option<Vec<String>>,
         env: Option<Vec<String>>,
         current_dir: Option<String>,
     }
     impl CommandBuilder {
+        pub fn build(&mut self) -> Result<#ident, Box<dyn Error>> {
+            if None == self.executable
+                || None == self.args
+                || None == self.env
+                || None == self.current_dir
+            {
+                return Err(Box::from("missing fields".to_owned()));
+            }
+            Ok(#ident{
+                executable: self.executable.as_ref().unwrap().to_owned(),
+                args: self.args.as_ref().unwrap().to_vec(),
+                env: self.env.as_ref().unwrap().to_vec(),
+                current_dir: self.current_dir.as_ref().unwrap().to_owned(),
+            })
+        }
         pub fn executable(&mut self, executable: String) -> &mut Self {
             self.executable = Some(executable);
             self
@@ -51,7 +64,7 @@ pub fn derive(input: TokenStream) -> TokenStream {
             self
         }
     }
-    };
+        };
 
     // 派生builder方法 并且添加 CommandBuilder 语法树
     let token = quote! {
