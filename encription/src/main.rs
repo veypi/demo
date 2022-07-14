2e88557f58bb64797154c75aa8b9ba1c0c2bba08
git <i@veypi.com>
2022-07-14 18:55:02 +0800

    aes gcm

--- encription/src/main.rs
+++ encription/src/main.rs
@@ -1,3 +1,24 @@
+//
+// main.rs
+// Copyright (C) 2022 veypi <i@veypi.com>
+// 2022-07-14 00:55
+// Distributed under terms of the Apache license.
+//
+
+use aes_gcm::aead::{Aead, NewAead};
+use aes_gcm::{Aes256Gcm, Key, Nonce}; // Or `Aes128Gcm`
 fn main() {
-    println!("Hello, world!");
+    let key = Key::from_slice(b"an example very very secret key.");
+    let cipher = Aes256Gcm::new(key);
+
+    let nonce = Nonce::from_slice(b"unique nonce"); // 96-bits; unique per message
+
+    let ciphertext = cipher
+        .encrypt(nonce, b"plaintext message".as_ref())
+        .expect("encryption failure!"); // NOTE: handle this error to avoid panics!
+    let plaintext = cipher
+        .decrypt(nonce, ciphertext.as_ref())
+        .expect("decryption failure!"); // NOTE: handle this error to avoid panics!
+
+    assert_eq!(&plaintext, b"plaintext message");
 }
